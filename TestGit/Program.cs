3ad80317e5759a95b767b6b8aa250bb6a2819edb
denvysze <szekingtung@gmail.com>
2014-09-26 14:23:50 +0800

    test 221

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -8,7 +8,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there");
+            Console.WriteLine("Hello there2");
         }
     }
 }
